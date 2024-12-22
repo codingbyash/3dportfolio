25fb51c55862dbf768d7e4b615ec8b58f12a4d82
ashishkr <singhashishgg2002@gmail.com>
2024-12-22 12:53:09 +0530

    adding

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require("path");
 require('dotenv').config();
 
 
@@ -15,6 +16,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Could not connect to MongoDB:', error));
