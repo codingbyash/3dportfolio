7a7deeeccbe493977c9b88dfc029c029e0352dd8
ashishkr <singhashishgg2002@gmail.com>
2025-01-15 01:48:11 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+
 app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+});
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Could not connect to MongoDB:', error));
